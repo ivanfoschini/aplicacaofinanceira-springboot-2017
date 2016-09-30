b23c6d66822d8293d8742339ca3e38b3f51569af
Ivan João Foschini <ivan@dc.ufscar.br>
2016-09-30 17:25:04 -0300

    Inclusão dos métodos delete, findAll e findById na classe BancoController.

--- src/main/java/aplicacaofinanceira/controller/BancoController.java
+++ src/main/java/aplicacaofinanceira/controller/BancoController.java
@@ -3,12 +3,14 @@ package aplicacaofinanceira.controller;
 import aplicacaofinanceira.model.Banco;
 import aplicacaofinanceira.service.BancoService;
 import aplicacaofinanceira.validation.ValidationUtil;
+import java.util.Collection;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,17 +23,50 @@ public class BancoController extends BaseController {
     private BancoService bancoService;
 
     @RequestMapping(
+            value = "/api/bancos/{id}",
+            method = RequestMethod.DELETE)
+    public ResponseEntity<Banco> delete(@PathVariable("id") Long id) {
+        bancoService.delete(id);
+
+        return new ResponseEntity<Banco>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(
+            value = "/api/bancos",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<Banco>> findAll() {
+        Collection<Banco> bancos = bancoService.findAll();
+
+        return new ResponseEntity<Collection<Banco>>(bancos, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/api/bancos/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Banco> findById(@PathVariable("id") Long id) {
+        Banco banco = bancoService.findById(id);
+
+        if (banco == null) {
+            return new ResponseEntity<Banco>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Banco>(banco, HttpStatus.OK);
+    }
+
+    @RequestMapping(
             value = "/api/bancos",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> createBanco(@RequestBody @Valid Banco banco, BindingResult bindingResult) {        
+    public ResponseEntity<Object> insert(@RequestBody @Valid Banco banco, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return new ResponseEntity<>(ValidationUtil.getBeanValidationErrors(bindingResult), HttpStatus.UNPROCESSABLE_ENTITY);  
+            return new ResponseEntity<>(ValidationUtil.getBeanValidationErrors(bindingResult), HttpStatus.UNPROCESSABLE_ENTITY);
         } else {
-            Banco savedBanco = bancoService.insert(banco);        
-        
-            return new ResponseEntity<>(savedBanco, HttpStatus.CREATED);    
+            Banco savedBanco = bancoService.insert(banco);
+
+            return new ResponseEntity<>(savedBanco, HttpStatus.CREATED);
         }
     }
 }
--- src/main/java/aplicacaofinanceira/controller/BaseController.java
+++ src/main/java/aplicacaofinanceira/controller/BaseController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -23,29 +24,51 @@ public class BaseController {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Map<String, Object>> handleDataIntegrityViolationException(Exception exception, HttpServletRequest request) {
-        logger.error("Exception start.");
-        logger.error("Exception: ", exception);
+        logExceptionStart(exception);
 
         ExceptionAttributes exceptionAttributes = new DefaultExceptionAttributes();
 
         Map<String, Object> responseBody = exceptionAttributes.getExceptionAttributes(messageSource.getMessage("bancoUniqueError", null, null), exception, request, HttpStatus.BAD_REQUEST);
 
-        logger.error("Exception end.");
+        logExceptionEnd();
+
+        return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.BAD_REQUEST);
+    }
+    
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Map<String, Object>> handleEmptyResultDataAccessException(Exception exception, HttpServletRequest request) {
+        logExceptionStart(exception);
+        
+        ExceptionAttributes exceptionAttributes = new DefaultExceptionAttributes();
+
+        Map<String, Object> responseBody = exceptionAttributes.getExceptionAttributes(messageSource.getMessage("bancoNotFoundError", null, null), exception, request, HttpStatus.BAD_REQUEST);
+
+        logExceptionEnd();
 
         return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.BAD_REQUEST);
     }
     
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Map<String, Object>> handleHttpMessageNotReadableException(Exception exception, HttpServletRequest request) {
-        logger.error("Exception start.");
-        logger.error("Exception: ", exception);
-
+        logExceptionStart(exception);
+        
         ExceptionAttributes exceptionAttributes = new DefaultExceptionAttributes();
 
         Map<String, Object> responseBody = exceptionAttributes.getExceptionAttributes(messageSource.getMessage("generalBadRequest", null, null), exception, request, HttpStatus.BAD_REQUEST);
 
-        logger.error("Exception end.");
-
+        logExceptionEnd();
+        
         return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.BAD_REQUEST);
+    }
+    
+    protected void logExceptionStart(Exception exception) {
+        logger.error("====================");
+        logger.error("Exception start.");
+        logger.error("Exception: ", exception);        
+    }
+    
+    protected void logExceptionEnd() {
+        logger.error("Exception end.");
+        logger.error("====================");
     }
 }
