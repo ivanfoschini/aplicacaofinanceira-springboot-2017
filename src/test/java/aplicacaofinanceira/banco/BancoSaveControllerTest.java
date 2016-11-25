4ed0b1e1390a5e154fc8dce74a29554340b3102c
Ivan João Foschini <ivan@dc.ufscar.br>
2016-11-25 11:59:20 -0200

    Implementação dos testes de inserção da entidade Banco.

--- src/test/java/aplicacaofinanceira/banco/BancoSaveControllerTest.java
+++ src/test/java/aplicacaofinanceira/banco/BancoSaveControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,12 +20,54 @@ public class BancoSaveControllerTest extends AbstractControllerTest {
     public void setUp() {
         super.setUp();
     }
+    
+    @Test
+    public void testSaveComUsuarioNaoAutorizado() throws Exception {
+        String uri = TestUtil.BANCOS_URI;
+        
+        Banco banco = TestUtil.createValidBanco();
+        
+        String inputJson = super.mapToJson(banco);
 
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, TestUtil.getFuncionarioAuthorization())
+                        .content(inputJson))                
+                .andReturn();
+
+        int status = result.getResponse().getStatus();
+        
+        Assert.assertEquals(HttpStatus.FORBIDDEN.value(), status);
+    }
+
+    @Test
+    public void testSaveComUsuarioComCredenciaisIncorretas() throws Exception {
+        String uri = TestUtil.BANCOS_URI;
+        
+        Banco banco = TestUtil.createValidBanco();
+        
+        String inputJson = super.mapToJson(banco);
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, TestUtil.getAdminAuthorizationWithWrongPassword())
+                        .content(inputJson))                
+                .andReturn();
+
+        int status = result.getResponse().getStatus();
+        
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED.value(), status);
+    }  
+    
     @Test
     public void testSaveComSucesso() throws Exception {
         String uri = TestUtil.BANCOS_URI;
         
-        Banco banco = TestUtil.createBanco();
+        Banco banco = TestUtil.createValidBanco();
         
         String inputJson = super.mapToJson(banco);
 
@@ -42,7 +85,28 @@ public class BancoSaveControllerTest extends AbstractControllerTest {
         Banco savedBanco = super.mapFromJson(content, Banco.class);        
         banco.setId(savedBanco.getId());
         
-        Assert.assertEquals(201, status);
+        Assert.assertEquals(HttpStatus.CREATED.value(), status);
         Assert.assertEquals(banco, savedBanco);
     }    
+    
+    @Test
+    public void testSaveSemCamposObrigatorios() throws Exception {
+        String uri = TestUtil.BANCOS_URI;
+        
+        Banco banco = TestUtil.createNullBanco();
+        
+        String inputJson = super.mapToJson(banco);
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, TestUtil.getAdminAuthorization())
+                        .content(inputJson))                
+                .andReturn();
+
+        int status = result.getResponse().getStatus();
+        
+        Assert.assertEquals(HttpStatus.UNPROCESSABLE_ENTITY.value(), status);
+    }
 }
--- src/test/java/aplicacaofinanceira/util/TestUtil.java
+++ src/test/java/aplicacaofinanceira/util/TestUtil.java
@@ -11,7 +11,7 @@ public class TestUtil {
     private static final String BANCO_NOME = "Banco do Brasil";
     private static final Integer BANCO_NUMERO = 1;
     
-    public static Banco createBanco() {
+    public static Banco createValidBanco() {
         Banco banco = new Banco();
         banco.setNumero(BANCO_NUMERO);
         banco.setCnpj(BANCO_CNPJ);
@@ -20,7 +20,24 @@ public class TestUtil {
         return banco;
     }
     
+    public static Banco createNullBanco() {
+        Banco banco = new Banco();
+        banco.setNumero(null);
+        banco.setCnpj(null);
+        banco.setNome(null);
+        
+        return banco;
+    }
+    
     public static String getAdminAuthorization() {
         return "Basic " + Base64Utils.encodeToString("admin:admin".getBytes());
+    }
+    
+    public static String getAdminAuthorizationWithWrongPassword() {
+        return "Basic " + Base64Utils.encodeToString("admin:adminadmin".getBytes());
+    }
+    
+    public static String getFuncionarioAuthorization() {
+        return "Basic " + Base64Utils.encodeToString("funcionario:funcionario".getBytes());
     }
 }
