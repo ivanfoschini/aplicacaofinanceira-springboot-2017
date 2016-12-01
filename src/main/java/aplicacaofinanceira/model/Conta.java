9cce0761e882ca1daa3dee115143be30abba7cb7
Ivan João Foschini <ivan@dc.ufscar.br>
2016-12-01 14:08:37 -0200

    Melhorias no código.

--- src/main/java/aplicacaofinanceira/model/Conta.java
+++ src/main/java/aplicacaofinanceira/model/Conta.java
@@ -55,7 +55,6 @@ public abstract class Conta implements Serializable {
     @NotNull(message = "{contaDataDeAberturaNaoPodeSerNula}")
     @Column(name = "data_de_abertura", nullable = false)
     @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
-
     @JsonView({ContaCorrenteViews.ContaCorrenteSimple.class, ContaPoupancaViews.ContaPoupancaSimple.class})
     private LocalDate dataDeAbertura;
         
